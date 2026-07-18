396c5ab7f469700d117874c137b10f18139d849e
TheWarHawk2005 <louis.h.harrison@gmail.com>
2026-07-18 11:43:53 -0400

    push demo to homepage

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 console.log('Website programmed by Lou Harrison.')
+console.log('Updated 7/18/2026')
+scrollToHeader()
+
+
+document.getElementById('navbar-contact-cta').addEventListener('click', e => {
+	window.location.hash = '#contact'
+	scrollToHeader(300)
+})
+
+async function scrollToHeader(delay = 1000) {
+    setTimeout(() => {
+        // check for hash in url
+        const scrollToId = window.location.hash.slice(1, window.location.hash.length)
+        if (scrollToId) {
+            document.getElementById(scrollToId).scrollIntoView({ behavior: "smooth" })
+            console.log('Scrolled to', scrollToId);
+        } else {
+			window.scrollTo(0,0)
+            console.log('Provide a #section to scroll a section into view on page load')
+        }
+    },
+        delay)
+}
